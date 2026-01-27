16d0b6447d321f1b536993561e50a3ec86213504
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-01-27 07:56:44 -0500

    feat: edit button

--- js/feed.js
+++ js/feed.js
@@ -6,8 +6,8 @@ const ideas = localStorage.getItem('ideahub_ideas');
 const ideasList = JSON.parse(ideas);
 const user = localStorage.getItem('loggedUser');
 const currentUser = JSON.parse(user)
-
 const themeToggle = document.getElementById('themeToggle');
+const feed = document.getElementById('feed')
 
 themeToggle.addEventListener('click', () => {
     const currentTheme = document.documentElement.getAttribute("data-theme");
@@ -15,9 +15,7 @@ themeToggle.addEventListener('click', () => {
     document.documentElement.setAttribute('data-theme', newTheme);
 
     themeToggle.textContent = newTheme === 'dark' ? '☀️' : '🌙';
-
 });
-
 
 function renderFeed(ideas) {
     const ideasFeed = document.getElementById('feed');
@@ -59,17 +57,29 @@ function renderFeed(ideas) {
             btn.innerHTML=`Edit<img src="assets/edit.svg">`
 
             buttonSpace.appendChild(btn)
-            
-        }
-
-                
+        }     
     })
     authorsSet.forEach(element => {
         const authorOption = document.createElement('option');
         authorOption.innerHTML = `<option value="${element}">${element}</option>`
         authorSelect.appendChild(authorOption)
     });
-    
 }
 
 renderFeed(ideasList)
+
+feed.addEventListener('click', (e) =>{
+    console.log('entra')
+    const modal = document.getElementById('editModal')
+    if (e.target.closest('.edit-btn')) {
+        modal.style.display = "block";
+        console.log('boton')
+        const card = e.target.closest('.idea-card');
+        const id = card.dataset.id;
+        console.log(card)
+    }
+})
+
+const editBtn = document.querySelectorAll('.edit-btn')
+console.log(editBtn)
+
