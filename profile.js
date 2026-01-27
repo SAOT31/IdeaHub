79a489da5323927ae6993a4cb222c383f8f152ee
Jose Rivera <miguel.quiroz0310@gmail.com>
2026-01-27 14:36:04 -0500

    feat: Render the user with data burned into the profile.js file

--- profile.js
+++ profile.js
@@ -1,5 +1,6 @@
 const nameUser = document.getElementById("name")
 const emailUser = document.getElementById("email")
+const totIdeas = document.getElementById("totIdeas")
 
 
 //  Theme Change
@@ -16,10 +17,74 @@ toggleBtn.addEventListener("click", () => {
     html.setAttribute("data-theme", "dark");
     toggleBtn.textContent = "☀️ ";
   }
-});
+})
+
+// List users
+
+const users = [
+  {
+    "name": "pepito perez",
+    "username":"peperez",
+    "email":"pe_perez@gmail.com",
+    "totalIdeas": "14", // Reemplazar por la variable que me almacena la longitud de la lista ideas
+    "ideas":[
+      {
+        "id":"1234",
+        "creator":this.username,
+        "title":"QUIEN MONDA ES DORIAN?",
+        "description":"Mondá pa tu jopo maricon",
+        "category":"Urgente",
+        "createAt": "hoy"
+
+      }
+    ]
+  },
+  {
+    "name": "pepa pelaez",
+    "username":"pepita",
+    "email":"corozo@gmail.com",
+    "totalIdeas": "41", // Reemplazar por la variable que me almacena la longitud de la lista ideas
+    "ideas":[
+      {
+        "id":"4321",
+        "creator":this.username,
+        "title":"QUIEN MONDA ES DORIAN?",
+        "description":"Mondá pa tu jopo maricon",
+        "category":"Low key",
+        "createAt": "mañana"
+
+      }
+    ]
+  }
+]
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  localStorage.setItem("Users", JSON.stringify(users))
+
+  let userName = users[1].name
+  let userEmail = users[1].email
+  let userTotalIdeas = users[1].totalIdeas
+
+  console.log(users[0].name)
+  nameUser.innerHTML = `<p id="name"><strong>Name:</strong> ${userName} </p>`
+  emailUser.innerHTML = `<p id="email"><strong>Email:</strong> ${userEmail}</p>`
+  totIdeas.innerHTML = `<p id="totIdeas"><strong>Total Ideas:</strong><span> ${userTotalIdeas}</span></p>`
+})
+
+
+
+
+
 
 // User Information
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
     const users = JSON.parse(localStorage.getItem("Users")) || [];
     console.log(users)
 
@@ -29,4 +94,4 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(users[0].name)
     nameUser.innerHTML = `<p id="name"><strong>Name:</strong> ${userName} </p>`
     emailUser.innerHTML = `<p id="email"><strong>Email:</strong> ${userEmail}</p>`
-})
+})`*/
